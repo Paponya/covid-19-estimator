3e402305d4bef5b918ac66c1a357a31573357f5f
Paponya <langat.paponya@gmail.com>
2020-04-11 17:57:15 +0200

    challenge 1,2 and 3

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,50 @@
-const covid19ImpactEstimator = (data) => data;
+/* eslint-disable */
 
-export default covid19ImpactEstimator;
+function futureImpactEstimate(reportedCases, estimateFactor, timeToElapse, periodType, totalHospitalBeds, avgDailyIncomeInUSD, avgDailyIncomePopulation) {
+    const period = convertTimeToElapse(timeToElapse, periodType);
+    const currentlyInfected = reportedCases * estimateFactor;
+    const infectionsByRequestedTime = currentlyInfected * (Math.pow(2, Math.trunc(period / 3)))
+    const severeCasesByRequestedTime = Math.trunc(0.15 * infectionsByRequestedTime)
+    const hospitalBedsByRequestedTime = (Math.trunc(0.35 * totalHospitalBeds)) - severeCasesByRequestedTime;
+    const casesForICUByRequestedTime = Math.trunc(0.05 * infectionsByRequestedTime)
+    const casesForVentilatorsByRequestedTime = Math.trunc(0.02 * infectionsByRequestedTime)
+    const dollarsInFlight = Math.trunc((infectionsByRequestedTime * avgDailyIncomePopulation * avgDailyIncomeInUSD) / period);
+    const impact = {
+        currentlyInfected: currentlyInfected,
+        infectionsByRequestedTime: infectionsByRequestedTime,
+        severeCasesByRequestedTime: severeCasesByRequestedTime,
+        hospitalBedsByRequestedTime: hospitalBedsByRequestedTime,
+        casesForICUByRequestedTime: casesForICUByRequestedTime,
+        casesForVentilatorsByRequestedTime: casesForVentilatorsByRequestedTime,
+        dollarsInFlight: dollarsInFlight
+    }
+    return impact;
+}
+function convertTimeToElapse(timeToElapse, periodType) {
+    let period
+    switch (periodType) {
+        case 'days':
+            period = timeToElapse
+            break;
+        case 'weeks':
+            period = timeToElapse * 7
+            break;
+        case 'months':
+            period = timeToElapse * 30
+            break;
+        default:
+    }
+    return period
+}
+const covid19ImpactEstimator = (data) => {
+    const { region, periodType, timeToElapse, reportedCases, totalHospitalBeds } = data.body
+    const { avgDailyIncomeInUSD, avgDailyIncomePopulation} = region;
+    const response = {
+        data: data.body, //input data
+        impact: futureImpactEstimate(reportedCases, 10, timeToElapse, periodType, totalHospitalBeds, avgDailyIncomeInUSD, avgDailyIncomePopulation), // best case estimates
+        severeImpact: futureImpactEstimate(reportedCases, 50, timeToElapse, periodType, totalHospitalBeds, avgDailyIncomeInUSD, avgDailyIncomePopulation) // worst case estimates
+    }
+    return response;
+};
+
+module.exports  = covid19ImpactEstimator;
---
+++ src/index.js
@@ -0,0 +1,33 @@
+/* eslint-disable */
+const express = require('express');
+const fs = require('fs')
+const morgan = require('morgan')
+const path = require('path')
+const xml2js = require('xml2js');
+const covid19ImpactEstimator = require('./estimator')
+const validateRequest = require('./validator')
+const app = express();
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'logs/access.log'), { flags: 'a' })
+app.use(express.json());
+app.use(morgan('tiny', { stream: accessLogStream }))
+app.use('/api/v1/on-covid19/logs', express.static(path.join(__dirname, 'logs/access.log')))
+app.post('/api/v1/on-covid-19', (req, res) => {
+    jsonResponse(req, res)
+});
+app.post('/api/v1/on-covid-19/json', (req, res) => {
+    jsonResponse(req, res)
+});
+app.post('/api/v1/on-covid-19/xml', (req, res) => {
+    validateRequest(req, res)
+    const builder = new xml2js.Builder();
+    const response = builder.buildObject(covid19ImpactEstimator(req));
+    res.type('application/xml');
+    res.send(response);
+})
+function jsonResponse(req, res) {
+    validateRequest(req, res)
+    const response = covid19ImpactEstimator(req)
+    res.send(response);
+}
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`Listening on port ${port}`))
